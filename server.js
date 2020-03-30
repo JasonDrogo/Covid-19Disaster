ef08e4be376b79efb38c0be4a1fecd9fe65aeaeb
pk856j <pk856j@att.com>
2020-03-30 12:47:34 +0530

    dev

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const path = require("path");
+const app = express();
+const cors = require("cors");
+const axios = require("axios");
+require("dotenv").config();
+app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+    next();
+});
+app.get('/users', (req, res) => {
+    axios({
+        "method": "GET",
+        "url": "https://covid-193.p.rapidapi.com/statistics",
+        "headers": {
+            "content-type": "application/octet-stream",
+            "x-rapidapi-host": "covid-193.p.rapidapi.com",
+            "x-rapidapi-key": process.env.KEY
+        }
+    })
+        .then((response) => res.json(response.data['response']))
+        .catch((error) => {
+            console.log(error)
+        })
+})
+app.use(express.static(path.join(__dirname, "client/dist")));
+// Anything that doesn't match the above, send back index.html
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname + "/client/dist/index.html"));
+});
+
+app.use(bodyParser.json());
+app.use(cors());
+// router(app);
+app.listen(4000, () => console.log("express server started"));
