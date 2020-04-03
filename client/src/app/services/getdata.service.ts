14d7dae382ccc8f717660fae99335a30f6e0f2fe
pk856j <pk856j@att.com>
2020-04-03 11:07:21 +0530

    'new_dev'

--- client/src/app/services/getdata.service.ts
+++ client/src/app/services/getdata.service.ts
@@ -10,30 +10,19 @@ import { IDoughnutData } from '../model/doughnut.model';
 })
 export class GetdataService {
   AllCountriesData: ICountryCase[] = [];
-  length: number;
   constructor(private _http: HttpClient) { }
-
-
   public getLiveData(): Observable<any> {
     const headers = new HttpHeaders({ "Access-Control-Allow-Credentials": "true" })
     return this._http.get<any>("http://localhost:3001/users", { headers });
   }
   public saveData(data: ICountryCase[]) {
-    this.length = data.length;
-
     this.AllCountriesData = [...data];
-    // this.AllCountriesData.sort((a, b) => { return b.cases.total - a.cases.total });
-
-
   }
   public getDoughnutData() {
     return this.AllCountriesData[0];
   }
 
   public getSavedData(): ICountryCase[] {
-
     return this.AllCountriesData;
-
   }
-
 }
